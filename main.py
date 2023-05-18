a81cfdb1ac7cbbdb184177d2d5d0d41d1f5fe6f4
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-05-18 12:28:34 -0700

    Grid with validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor, QFont, QIntValidator
 from PyQt6.QtWidgets import (QWidget, QLabel,
                              QLineEdit, QApplication, QGridLayout, QFrame)
 
@@ -8,41 +9,49 @@ class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.grid = None
         self.initUI()
 
+
+    def resizeEvent(self, event):
+        # Get the current window size
+        self.adjustWindowSize()
+        self.adjustFontSizes()
+        self.adjustGridLayoutSpacing()
+
+
+        event.accept()
+
     def initUI(self):
-        grid = QGridLayout()
-        self.setLayout(grid)
-        grid.setSpacing(5)
 
-        tiles = [[_ for _ in range(1, 10)] for _ in range(1, 10)]
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+        self.grid.setSpacing(5)
+        self.setProperty("class", "window")  # Set background color
 
-        for row in range(len(tiles)):
-            for col in range(len(tiles[row])):
-                tile = tiles[row][col]
-                if tile == '':
-                    continue
-                tile_label = QLineEdit(str(tile))
+
+        # tiles = [[_ for _ in range(1, 10)] for _ in range(1, 10)]
+
+        for row in range(9):
+            for col in range(9):
+                # tile = tiles[row][col]
+                # if tile == '':
+                #     continue
+                tile_label = QLineEdit()
                 tile_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-                grid.addWidget(tile_label, row, col)
 
-                if col % 3 == 2 and col != len(tiles[row]) - 1:
-                    line = QFrame()
-                    line.setFrameShape(QFrame.Shape.VLine)
-                    grid.addWidget(line, row, col + 1)
-                    grid.setRowStretch(row, 1)
+                validator = QIntValidator(1, 9)
+                tile_label.setValidator(validator)
 
-            if row % 3 == 2 and row != len(tiles) - 1:
-                for col in range(len(tiles[row])):
-                    line = QFrame()
-                    line.setFrameShape(QFrame.Shape.HLine)
-                    grid.addWidget(line, row + 1, col)
-                    grid.setColumnStretch(col, 1)
+                self.grid.addWidget(tile_label, row, col)
 
-        self.setGeometry(300, 300, 350, 250)
+                if row // 3 % 2 == col // 3 % 2:
+                    tile_label.setProperty("class", "box1")
+                else:
+                    tile_label.setProperty("class", "box2")
+
+        self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('Sudoku Solver')
-        self.show()
 
     # def onChanged(self, text):
     #
@@ -50,9 +59,44 @@ class MainWindow(QWidget):
     #     self.lbl.adjustSize()
 
 
+    def adjustWindowSize(self):
+        current_size = self.size()
+        # Adjust the width and height to maintain the aspect ratio
+        aspect_ratio = 1  # Set your desired aspect ratio here
+        if current_size.width() != current_size.height() * aspect_ratio:
+            new_width = current_size.height() * aspect_ratio
+            self.resize(new_width, current_size.height())
+
+    def adjustFontSizes(self):
+        windowWidth = self.width()
+
+        # Calculate the font size based on the window width
+        fontSize = int(windowWidth / 25)  # Adjust the division factor to your preference
+
+        # Create a QFont object with the desired font family and size
+        font = QFont("Beirut", fontSize)
+
+        # Iterate through all QLineEdit widgets and set the font
+        for child in self.findChildren(QLineEdit):
+            child.setFont(font)
+
+
+    def adjustGridLayoutSpacing(self):
+        windowWidth = self.width()
+
+        # Calculate the spacing based on the window width
+        spacing = int(windowWidth / 100)  # Adjust the division factor to your preference
+
+        # Set the spacing for the QGridLayout
+        self.grid.setSpacing(spacing)
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
+    with open("styles.css", "r") as file:
+        app.setStyleSheet(file.read())
+
+    window.show()
     sys.exit(app.exec())
 
 
