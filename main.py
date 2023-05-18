5ed2bce64d2d84906f4f0f3c825ef48b130e4ae3
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-05-18 11:32:16 -0700

    Broken Lines

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-import numpy as np
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QWidget, QLabel,
-        QLineEdit, QApplication, QGridLayout)
+                             QLineEdit, QApplication, QGridLayout, QFrame)
 
 
 class MainWindow(QWidget):
@@ -11,11 +11,11 @@ class MainWindow(QWidget):
 
         self.initUI()
 
-
     def initUI(self):
         grid = QGridLayout()
         self.setLayout(grid)
-        
+        grid.setSpacing(5)
+
         tiles = [[_ for _ in range(1, 10)] for _ in range(1, 10)]
 
         for row in range(len(tiles)):
@@ -24,10 +24,23 @@ class MainWindow(QWidget):
                 if tile == '':
                     continue
                 tile_label = QLineEdit(str(tile))
+                tile_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
                 grid.addWidget(tile_label, row, col)
 
+                if col % 3 == 2 and col != len(tiles[row]) - 1:
+                    line = QFrame()
+                    line.setFrameShape(QFrame.Shape.VLine)
+                    grid.addWidget(line, row, col + 1)
+                    grid.setRowStretch(row, 1)
+
+            if row % 3 == 2 and row != len(tiles) - 1:
+                for col in range(len(tiles[row])):
+                    line = QFrame()
+                    line.setFrameShape(QFrame.Shape.HLine)
+                    grid.addWidget(line, row + 1, col)
+                    grid.setColumnStretch(col, 1)
+
         self.setGeometry(300, 300, 350, 250)
-        # self.move(300, 300)
         self.setWindowTitle('Sudoku Solver')
         self.show()
 
@@ -38,7 +51,6 @@ class MainWindow(QWidget):
 
 
 def main():
-
     app = QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec())
