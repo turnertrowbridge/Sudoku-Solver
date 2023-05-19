538e371aef14404e520d23b978a072b4e9c30f7a
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-05-18 19:05:08 -0700

    styling finished for now

--- main.py
+++ main.py
@@ -1,92 +1,89 @@
 import sys
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtGui import QFont, QIntValidator
-from PyQt6.QtWidgets import (QWidget, QLineEdit, QApplication, QGridLayout, QSizePolicy)
+from PyQt6.QtWidgets import QWidget, QLineEdit, QApplication, QGridLayout, QVBoxLayout, QLabel, QHBoxLayout, \
+    QPushButton, QSizePolicy
 
 
 class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        # self.setFixedSize(600, 600)
-        self.line_edit_size = None
-        self.grid = None
         self.initUI()
 
-    def resizeEvent(self, event):
-        # Get the current window size
-        self.adjustWindowSize()
-        self.adjustFontSizes()
-        # self.adjustLineEditSizes()
-        event.accept()
-
     def initUI(self):
+        main_layout = QVBoxLayout(self)
+        self.setWindowTitle('Sudoku Solver')
 
-        self.grid = QGridLayout()
-        self.setLayout(self.grid)
+        # Labels
+        word_layout = QHBoxLayout()
+        title_label = QLabel("Sudoku Solver", alignment=Qt.AlignmentFlag.AlignLeft)
+        title_label.setProperty("class", "title")
+        info_label = QLabel("Add current sudoku numbers to grid and hit calculate to solve the board.", alignment=Qt.AlignmentFlag.AlignCenter)
+        info_label.setProperty("class", "info")
+        word_layout.addWidget(title_label)
+        word_layout.addWidget(info_label)
+        main_layout.addLayout(word_layout)
+
+        # Grid
+        grid_layout = QGridLayout()
+        main_layout.addLayout(grid_layout)
         self.setProperty("class", "window")  # Set background color
-        self.grid.setSpacing(0)  # Set the spacing between items in the grid to 0
-        self.grid.setContentsMargins(0, 0, 0, 0)
-
-        self.line_edit_size = 50
-
-        # tiles = [[_ for _ in range(1, 10)] for _ in range(1, 10)]
+        grid_layout.setSpacing(0)  # Set the spacing between items in the grid to 0
+        grid_layout.setContentsMargins(0, 0, 0, 0)
 
         for row in range(9):
             for col in range(9):
-                # tile = tiles[row][col]
-                # if tile == '':
-                #     continue
-                tile_label = QLineEdit()
-                tile_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                tile_label = QLineEdit(alignment=Qt.AlignmentFlag.AlignCenter)
+                tile_label.setValidator(QIntValidator(1, 9))
                 tile_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-                validator = QIntValidator(1, 9)
-                tile_label.setValidator(validator)
-                # tile_label.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+                grid_layout.addWidget(tile_label, row, col)
 
-                self.grid.addWidget(tile_label, row, col)
+                box_class = "box1" if (row // 3 % 2 == col // 3 % 2) else "box2"
+                if (row + col) % 2 != 0:
+                    box_class += "-odd"
+                tile_label.setProperty("class", box_class)
 
-                if row // 3 % 2 == col // 3 % 2:
-                    if (row + col) % 2 == 0:
-                        tile_label.setProperty("class", "box1")
-                    else:
-                        tile_label.setProperty("class", "box1-odd")
-                else:
-                    if (row + col) % 2 == 0:
-                        tile_label.setProperty("class", "box2")
-                    else:
-                        tile_label.setProperty("class", "box2-odd")
+        # Buttons
+        solve_button = QPushButton("Solve")
+        reset_button = QPushButton("Reset")
+        solve_button.clicked.connect(self.onSubmit)  # Connect the button's clicked signal to the onSubmit slot
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(solve_button)
+        button_layout.addWidget(reset_button)
+        main_layout.addLayout(button_layout)
 
-        self.setGeometry(100, 100, 100, 100)
-        self.setWindowTitle('Sudoku Solver')
+        self.adjustFontSizes()
 
-        self.adjustFontSizes()  # Call the method initially to set the font sizes
-
-    # def onChanged(self, text):
-    #
-    #     self.lbl.setText(text)
-    #     self.lbl.adjustSize()
-
-    def adjustWindowSize(self):
+    def resizeEvent(self, event):
         current_size = self.size()
-        # Adjust the width and height to maintain the aspect ratio
-        aspect_ratio = 1  # Set your desired aspect ratio here
-        if current_size.width() != current_size.height() * aspect_ratio:
-            new_width = current_size.height() * aspect_ratio
-            self.resize(new_width, current_size.height())
+        new_size = QSize(current_size.width(), current_size.height())  # Get the current size of the window
+
+        # Constrain the resizing to a diagonal aspect ratio
+        if new_size.width() != new_size.height():
+            if new_size.width() > new_size.height():
+                new_size.setWidth(new_size.height())
+            else:
+                new_size.setHeight(new_size.width())
+
+        self.resize(new_size)  # Set the new size of the window
+
 
     def adjustFontSizes(self):
-        windowWidth = self.width()
+        window_width = self.width()
+        font_size = int(window_width / 15)
+        font = QFont("Beirut", font_size)
 
-        # Calculate the font size based on the window width
-        fontSize = int(windowWidth / 15)  # Adjust the division factor to your preference
-
-        # Create a QFont object with the desired font family and size
-        font = QFont("Beirut", fontSize)
-
-        # Iterate through all QLineEdit widgets and set the font
         for child in self.findChildren(QLineEdit):
             child.setFont(font)
+
+    def onSubmit(self):
+        box_contents = []
+        for child in self.findChildren(QLineEdit):
+            text = child.text()
+            box_contents.append(text)
+
+        print(box_contents)
 
 
 def main():
